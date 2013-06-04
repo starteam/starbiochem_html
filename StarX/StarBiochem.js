f72502a1b5b47ca48651cc94eedfcc9c1d6884c1
Chuck <cshubert@mit.edu>
2013-06-04 15:54:26 -0400

    Minor errors from port from StarPractice

--- StarX/StarBiochem.js
+++ StarX/StarBiochem.js
@@ -1,4 +1,4 @@
-define(['../StarBiochem/main'], function (StarPractice) {
+define(['../StarBiochem/main'], function (StarBiochem) {
     return {
         configure: function (config) {
 //        	alert("StarBiochem.js:configure");
